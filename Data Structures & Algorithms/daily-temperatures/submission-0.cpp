7ab9d0fc077aa11eca7f6fd3da55be0fd51d5a4d
omkar-svg <omkargaikwadoneplus@gmail.com>
2026-06-13 18:19:51 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        stack<pair<int,int>>st;
+        vector<int>ans;
+        int n = temp.size();
+        for(int i=n-1;i>=0;i--){
+
+            while(!st.empty()&&st.top().first<=temp[i]){
+                st.pop();
+            }
+            if(st.empty())
+            ans.push_back(0);
+            else
+            ans.push_back(st.top().second-i);
+
+            st.push({temp[i],i});
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
